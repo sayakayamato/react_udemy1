480d86a6801c598042f03bd75d8709a045b01c63
sayakayamato <sayaka814cl@gmail.com>
2022-08-03 11:34:35 +0900

    スプレッド構文

--- index.js
+++ index.js
@@ -32,3 +32,31 @@ console.log(`${meat}と${masato}だよ`);
 //デフォルト値・引数
 const sayHello = (yourName = "ゲスト") => console.log(`こんにちは、${yourName}さん！`);
 sayHello("神谷");
+
+
+
+//スプレッド構文
+// 配列の展開
+const arr1 = [1,2];
+console.log(arr1);
+console.log(...arr1);
+
+// const sumFunk = (num1, num2) => console.log(num1 + num2);
+// sumFunk(arr1[0], arr1[1]);
+// sumFunk(...arr1);
+
+//まとめる
+const arr2 = [1,2,3,4,5];
+const [num1,num2, ...arr3] = arr2;
+console.log(num1);
+console.log(arr3);
+
+//配列のコピー、結合
+const arr4 = [10, 20];
+const arr5 = [30, 40];
+const arr6 = [...arr4];
+console.log(arr6);
+
+const arr7 = [...arr4, ...arr5];
+console.log(arr7);
+
